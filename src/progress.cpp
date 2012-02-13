71722664f646962da3a1f309c70ef371641b7fc6
Bruce Merry <bmerry@gmail.com>
2012-02-13 10:57:17 +0200

    Add forgotten progress meter files

---
+++ src/progress.cpp
@@ -0,0 +1,86 @@
+/**
+ * @file
+ *
+ * A thread-safe progress meter, modelled on boost::progress_display.
+ */
+
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <ostream>
+#include <string>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/locks.hpp>
+#include "progress.h"
+
+const unsigned int ProgressDisplay::totalTics;
+
+ProgressDisplay::ProgressDisplay(size_type total,
+                                 std::ostream &os,
+                                 const std::string &s1,
+                                 const std::string &s2,
+                                 const std::string &s3)
+: os(os), s1(s1), s2(s2), s3(s3)
+{
+    restart(total);
+}
+
+void ProgressDisplay::updateNextTic()
+{
+    size_type t = ticsShown + 1;
+    /* We want to compute total * t / totalTics (rounded down), but the
+     * intermediate result may overflow. So instead, we represent total
+     * as totalQ * totalTics + totalR. Then the value we want is
+     * totalQ * t + (totalR * t / totalTics), and this will not lead to
+     * overflow provided that totalTics^2 is small enough.
+     */
+    nextTic = totalQ * t + (totalR * t / totalTics);
+}
+
+void ProgressDisplay::restart(size_type total)
+{
+    current = 0;
+    ticsShown = 0;
+    this->total = total;
+    totalQ = total / totalTics;
+    totalR = total % totalTics;
+    os  << s1 << "0%   10   20   30   40   50   60   70   80   90   100%\n"
+        << s2 << "|----|----|----|----|----|----|----|----|----|----|\n"
+        << s3;
+    os.flush();
+
+    updateNextTic();
+}
+
+ProgressDisplay::size_type ProgressDisplay::operator+=(size_type increment)
+{
+    boost::lock_guard<boost::mutex> lock(mutex);
+    this->current += increment;
+
+    while (ticsShown < totalTics && this->current >= nextTic)
+    {
+        os << '*'; os.flush();
+        ticsShown++;
+        updateNextTic();
+        if (ticsShown == totalTics)
+            os << std::endl;
+    }
+    return this->current;
+}
+
+ProgressDisplay::size_type ProgressDisplay::operator++()
+{
+    return operator+=(1);
+}
+
+ProgressDisplay::size_type ProgressDisplay::count() const
+{
+    boost::lock_guard<boost::mutex> lock(mutex);
+    return current;
+}
+
+ProgressDisplay::size_type ProgressDisplay::expected_count() const
+{
+    return total;
+}
---
+++ src/progress.h
@@ -0,0 +1,60 @@
+/**
+ * @file
+ *
+ * A thread-safe progress meter, modelled on boost::progress_display.
+ */
+
+#ifndef PROGRESS_H
+#define PROGRESS_H
+
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <ostream>
+#include <iosfwd>
+#include <string>
+#include <boost/thread/mutex.hpp>
+#include <tr1/cstdint>
+#include <boost/noncopyable.hpp>
+
+/**
+ * A thread-safe progress meter. It supports 64-bit progress values.
+ */
+class ProgressDisplay : public boost::noncopyable
+{
+public:
+    typedef std::tr1::uintmax_t size_type;
+
+    explicit ProgressDisplay(size_type total,
+                             std::ostream &os = std::cout,
+                             const std::string &s1 = "\n",
+                             const std::string &s2 = "",
+                             const std::string &s3 = "");
+
+    void restart(size_type total);
+    size_type operator++();
+    size_type operator+=(std::tr1::uintmax_t increment);
+
+    size_type count() const;
+    size_type expected_count() const;
+
+private:
+    size_type current;
+    unsigned int ticsShown;
+    size_type nextTic;
+
+    size_type total;
+    size_type totalQ;
+    size_type totalR;
+
+    mutable boost::mutex mutex;
+    std::ostream &os;
+    const std::string s1, s2, s3;
+
+    static const unsigned int totalTics = 51;
+
+    void updateNextTic();
+};
+
+#endif /* !PROGRESS_H */
