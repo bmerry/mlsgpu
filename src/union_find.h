6ffb1b1e9d5f60379aaaaeadf9731250272ebff3
Bruce Merry <bmerry@gmail.com>
2012-02-21 09:54:41 +0200

    Document and test UnionFind

--- src/union_find.h
+++ src/union_find.h
@@ -15,23 +15,51 @@
 #include <boost/static_assert.hpp>
 #include <boost/type_traits/is_signed.hpp>
 
+#if UNIT_TESTS
+class TestUnionFind;
+#endif
+
+/**
+ * Union-find data structure for efficient identification of components. It uses a tree
+ * with path compression and balancing by size, giving it O(alpha(N)) amortized time per
+ * operation.
+ *
+ * Each component has a canonical member, or @em root. Per-vertex storage is provided by
+ * the caller in a vector (or other random-access container).
+ */
 namespace UnionFind
 {
 
+/**
+ * A per-vertex element in the union-find data structure. If additional
+ * per-component information is desired, this class can be subclassed and @ref
+ * merge overloaded.
+ *
+ * @param Size A @b signed type with enough range to represent the number of
+ * elements in the graph.
+ */
 template<typename Size>
 class Node
 {
 public:
-    typedef Size size_type;
+    typedef Size size_type;     ///< Type used to store either parent index or node size
 
+    /**
+     * Default constructor. Creates a root node of size 1.
+     */
     Node() : parentSize(-1) {}
-    Node(size_type size) { setSize(size); }
 
+    /// Determines whether this node is the canonical root node for its component.
     bool isRoot() const
     {
         return parentSize < 0;
     }
 
+    /**
+     * The size of this component.
+     *
+     * @pre @ref isRoot() is true.
+     */
     size_type size() const
     {
         assert(parentSize < 0);
@@ -46,21 +74,36 @@ private:
         friend bool merge(NodeVector &nodes,
                           typename NodeVector::iterator::value_type::size_type a,
                           typename NodeVector::iterator::value_type::size_type b);
+#if UNIT_TESTS
+    friend class ::TestUnionFind;
+#endif
 
-    mutable size_type parentSize; ///< size if negative, parent if non-negative
+    /**
+     * Size if negative, parent if non-negative. This is mutable because path
+     * compression will alter it without making semantic changes to the
+     * structure.
+     */
+    mutable size_type parentSize;
 
+    /**
+     * Get the parent index in the tree.
+     *
+     * @pre @ref isRoot() is false.
+     */
     size_type parent() const
     {
         assert(parentSize >= 0);
         return parentSize;
     }
 
+    /// Sets the parent pointer in the tree, marking this node as non-root.
     void setParent(size_type p) const
     {
         assert(p >= 0);
         parentSize = p;
     }
 
+    /// Sets the size of this component, marking this node as a root.
     void setSize(size_type s)
     {
         BOOST_STATIC_ASSERT(boost::is_signed<size_type>::value);
@@ -69,6 +112,13 @@ private:
     }
 
 protected:
+    /**
+     * Update metadata about a component when another component is merged in.
+     * This method can be overloaded in subclasses to track additional
+     * component data.
+     *
+     * @param b     Root of a component that will become a child of this node.
+     */
     void merge(const Node &b)
     {
         assert(isRoot() && b.isRoot());
@@ -76,6 +126,15 @@ protected:
     }
 };
 
+/**
+ * Determines the root node of the component of a given node.
+ *
+ * @param nodes        Random access container of nodes giving a union-find structure.
+ * @param id           Index of the query node.
+ * @return Index of the unique root node that is in the same component as @a id.
+ * @note Although this function is semantically read-only, it performs path
+ * compression and so does modify the internals of @a nodes.
+ */
 template<typename NodeVector>
 typename NodeVector::iterator::value_type::size_type
 findRoot(const NodeVector &nodes, typename NodeVector::iterator::value_type::size_type id)
@@ -93,6 +152,15 @@ findRoot(const NodeVector &nodes, typename NodeVector::iterator::value_type::siz
     return root;
 }
 
+/**
+ * Combine two components. It is legal to call this function when the given
+ * nodes are already in the same component.
+ *
+ * @param nodes        Random access container of nodes giving a union-find structure.
+ * @param a, b         Two nodes (not necessarily roots) to combine.
+ * @retval @c true if two separate components were merged.
+ * @retval @c false if @a a and @a b were already in the same component.
+ */
 template<typename NodeVector>
 bool merge(NodeVector &nodes,
            typename NodeVector::iterator::value_type::size_type a,
---
+++ test/test_union_find.cpp
@@ -0,0 +1,131 @@
+/**
+ * @file
+ *
+ * Test code for @ref UnionFind.
+ */
+
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <vector>
+#include "../src/union_find.h"
+#include "testmain.h"
+
+/// Tests for @ref UnionFind
+class TestUnionFind : public CppUnit::TestFixture
+{
+    CPPUNIT_TEST_SUITE(TestUnionFind);
+    CPPUNIT_TEST(testIsRoot);
+    CPPUNIT_TEST(testMerge);
+    CPPUNIT_TEST(testFind);
+    CPPUNIT_TEST(testSize);
+    CPPUNIT_TEST_SUITE_END();
+
+private:
+    class Node : public UnionFind::Node<int>
+    {
+    public:
+        int x;     ///< Generic value that is summed over a component
+
+        void merge(const Node &b)
+        {
+            UnionFind::Node<int>::merge(b);
+            x += b.x;
+        }
+
+        Node() : x(0) {}
+    };
+
+    std::vector<Node> nodes;
+
+public:
+    virtual void setUp(); ///< Populates a union-find structure
+
+    void testIsRoot();
+    void testMerge();
+    void testFind();
+    void testSize();
+};
+CPPUNIT_TEST_SUITE_NAMED_REGISTRATION(TestUnionFind, TestSet::perBuild());
+
+void TestUnionFind::setUp()
+{
+    nodes.clear();
+    nodes.resize(9);
+    for (int i = 0; i < 9; i++)
+        nodes[i].x = i;
+    UnionFind::merge(nodes, 0, 1);
+    UnionFind::merge(nodes, 2, 3);
+    UnionFind::merge(nodes, 4, 6);
+    UnionFind::merge(nodes, 1, 3);
+    UnionFind::merge(nodes, 6, 7);
+    // Components are (0, 1, 2, 3), (4, 6, 7), (5), (8)
+}
+
+void TestUnionFind::testIsRoot()
+{
+    {
+        int roots = nodes[0].isRoot() + nodes[1].isRoot() + nodes[2].isRoot() + nodes[3].isRoot();
+        CPPUNIT_ASSERT_EQUAL(1, roots);
+    }
+    {
+        int roots = nodes[4].isRoot() + nodes[6].isRoot() + nodes[7].isRoot();
+        CPPUNIT_ASSERT_EQUAL(1, roots);
+    }
+    CPPUNIT_ASSERT(nodes[5].isRoot());
+    CPPUNIT_ASSERT(nodes[8].isRoot());
+}
+
+void TestUnionFind::testMerge()
+{
+    // Merge two that are already unified
+    bool merged = UnionFind::merge(nodes, 0, 2);
+    CPPUNIT_ASSERT(!merged);
+
+    // Merge two new ones
+    merged = UnionFind::merge(nodes, 1, 7);
+    CPPUNIT_ASSERT(merged);
+    int root = UnionFind::findRoot(nodes, 1);
+    CPPUNIT_ASSERT_EQUAL(root, UnionFind::findRoot(nodes, 7));
+    CPPUNIT_ASSERT_EQUAL(7, nodes[root].size());
+    CPPUNIT_ASSERT_EQUAL(23, nodes[root].x);
+}
+
+void TestUnionFind::testFind()
+{
+    int roots[4];
+    roots[0] = UnionFind::findRoot(nodes, 0);
+    roots[1] = UnionFind::findRoot(nodes, 4);
+    roots[2] = UnionFind::findRoot(nodes, 5);
+    roots[3] = UnionFind::findRoot(nodes, 8);
+    for (int i = 0; i < 4; i++)
+        for (int j = i + 1; j < 4; j++)
+            CPPUNIT_ASSERT(roots[i] != roots[j]);
+    CPPUNIT_ASSERT_EQUAL(roots[0], UnionFind::findRoot(nodes, 1));
+    CPPUNIT_ASSERT_EQUAL(roots[0], UnionFind::findRoot(nodes, 2));
+    CPPUNIT_ASSERT_EQUAL(roots[0], UnionFind::findRoot(nodes, 3));
+    CPPUNIT_ASSERT_EQUAL(roots[1], UnionFind::findRoot(nodes, 6));
+    CPPUNIT_ASSERT_EQUAL(roots[1], UnionFind::findRoot(nodes, 7));
+
+    // Check that path compression works
+    for (int i = 0; i < 9; i++)
+    {
+        CPPUNIT_ASSERT(nodes[i].isRoot() || nodes[nodes[i].parent()].isRoot());
+    }
+}
+
+void TestUnionFind::testSize()
+{
+    int roots[4];
+    roots[0] = UnionFind::findRoot(nodes, 0);
+    roots[1] = UnionFind::findRoot(nodes, 4);
+    roots[2] = UnionFind::findRoot(nodes, 5);
+    roots[3] = UnionFind::findRoot(nodes, 8);
+    CPPUNIT_ASSERT_EQUAL(4, nodes[roots[0]].size());
+    CPPUNIT_ASSERT_EQUAL(3, nodes[roots[1]].size());
+    CPPUNIT_ASSERT_EQUAL(1, nodes[roots[2]].size());
+    CPPUNIT_ASSERT_EQUAL(1, nodes[roots[3]].size());
+}
