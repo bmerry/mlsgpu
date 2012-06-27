c20a53518887ef3abda6cca87f50c76aea4589ea
Bruce Merry <bmerry@gmail.com>
2012-06-27 13:18:31 +0200

    Start on program for benchmarking sorting performance

---
+++ sorttest.cpp
@@ -0,0 +1,230 @@
+/**
+ * @file
+ *
+ * Artificial benchmark that sorts input PLY files by one coordinate and
+ * measures the maximum active set.
+ */
+
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <iostream>
+#include <cstdlib>
+#include <boost/program_options.hpp>
+#include <boost/io/ios_state.hpp>
+#include <boost/exception/all.hpp>
+#include <stxxl.h>
+#include "src/statistics.h"
+#include "src/splat_set.h"
+#include "src/fast_ply.h"
+#include "src/logging.h"
+#include "src/progress.h"
+#include "src/options.h"
+#include "src/provenance.h"
+
+namespace po = boost::program_options;
+
+namespace Option
+{
+    const char * const help = "help";
+    const char * const quiet = "quiet";
+    const char * const debug = "debug";
+
+    const char * const inputFile = "input-file";
+
+    const char * const statistics = "statistics";
+    const char * const statisticsFile = "statistics-file";
+
+    const char * const reader = "reader";
+};
+
+static void addCommonOptions(po::options_description &opts)
+{
+    opts.add_options()
+        ("help,h",                "Show help")
+        ("quiet,q",               "Do not show informational messages")
+        (Option::debug,           "Show debug messages");
+}
+
+static void addStatisticsOptions(po::options_description &opts)
+{
+    po::options_description statistics("Statistics options");
+    statistics.add_options()
+        (Option::statistics,                          "Print information about internal statistics")
+        (Option::statisticsFile, po::value<std::string>(), "Direct statistics to file instead of stdout (implies --statistics)");
+    opts.add(statistics);
+}
+
+static void addAdvancedOptions(po::options_description &opts)
+{
+    po::options_description advanced("Advanced options");
+    advanced.add_options()
+        (Option::reader,       po::value<Choice<FastPly::ReaderTypeWrapper> >()->default_value(FastPly::SYSCALL_READER), "File reader class (mmap | syscall)");
+    opts.add(advanced);
+}
+
+std::string makeOptions(const po::variables_map &vm)
+{
+    std::ostringstream opts;
+    for (po::variables_map::const_iterator i = vm.begin(); i != vm.end(); ++i)
+    {
+        const po::variable_value &param = i->second;
+        const boost::any &value = param.value();
+        if (param.empty()
+            || (value.type() == typeid(std::string) && param.as<std::string>().empty()))
+            opts << " --" << i->first;
+        else if (value.type() == typeid(std::vector<std::string>))
+        {
+            BOOST_FOREACH(const std::string &j, param.as<std::vector<std::string> >())
+            {
+                opts << " --" << i->first << '=' << j;
+            }
+        }
+        else
+        {
+            opts << " --" << i->first << '=';
+            if (value.type() == typeid(std::string))
+                opts << param.as<std::string>();
+            else if (value.type() == typeid(double))
+                opts << param.as<double>();
+            else if (value.type() == typeid(int))
+                opts << param.as<int>();
+            else if (value.type() == typeid(unsigned int))
+                opts << param.as<unsigned int>();
+            else if (value.type() == typeid(std::size_t))
+                opts << param.as<std::size_t>();
+            else if (value.type() == typeid(Choice<FastPly::ReaderTypeWrapper>))
+                opts << param.as<Choice<FastPly::ReaderTypeWrapper> >();
+            else
+                assert(!"Unhandled parameter type");
+        }
+    }
+    return opts.str();
+}
+
+void writeStatistics(const boost::program_options::variables_map &vm, bool force = false)
+{
+    if (force || vm.count(Option::statistics) || vm.count(Option::statisticsFile))
+    {
+        std::ostream *out;
+        std::ofstream outf;
+        if (vm.count(Option::statisticsFile))
+        {
+            const std::string &name = vm[Option::statisticsFile].as<std::string>();
+            outf.open(name.c_str());
+            out = &outf;
+        }
+        else
+        {
+            out = &std::cout;
+        }
+
+        boost::io::ios_exception_saver saver(*out);
+        out->exceptions(std::ios::failbit | std::ios::badbit);
+        *out << "sorttest version: " << provenanceVersion() << '\n';
+        *out << "sorttest variant: " << provenanceVariant() << '\n';
+        *out << "sorttest options:" << makeOptions(vm) << '\n';
+        *out << Statistics::Registry::getInstance();
+        *out << *stxxl::stats::get_instance();
+    }
+}
+
+static void usage(std::ostream &o, const po::options_description desc)
+{
+    o << "Usage: sorttest [options] input.ply [input.ply...]\n\n";
+    o << desc;
+}
+
+static po::variables_map processOptions(int argc, char **argv)
+{
+    po::positional_options_description positional;
+    positional.add(Option::inputFile, -1);
+
+    po::options_description desc("General options");
+    addCommonOptions(desc);
+    addStatisticsOptions(desc);
+    addAdvancedOptions(desc);
+
+    po::options_description hidden("Hidden options");
+    hidden.add_options()
+        (Option::inputFile, po::value<std::vector<std::string> >()->composing(), "input files");
+
+    po::options_description all("All options");
+    all.add(desc);
+    all.add(hidden);
+
+    try
+    {
+        po::variables_map vm;
+        po::store(po::command_line_parser(argc, argv)
+                  .style(po::command_line_style::default_style & ~po::command_line_style::allow_guessing)
+                  .options(all)
+                  .positional(positional)
+                  .run(), vm);
+
+        po::notify(vm);
+
+        if (vm.count(Option::help))
+        {
+            usage(std::cout, desc);
+            std::exit(0);
+        }
+        /* Using ->required() on the option gives an unhelpful message */
+        if (!vm.count(Option::inputFile))
+        {
+            std::cerr << "At least one input file must be specified.\n\n";
+            usage(std::cerr, desc);
+            std::exit(1);
+        }
+
+        return vm;
+    }
+    catch (po::error &e)
+    {
+        std::cerr << e.what() << "\n\n";
+        usage(std::cerr, desc);
+        std::exit(1);
+    }
+}
+
+static void run(const po::variables_map &vm)
+{
+}
+
+static void reportException(std::exception &e)
+{
+    std::cerr << '\n';
+
+    std::string *file_name = boost::get_error_info<boost::errinfo_file_name>(e);
+    if (file_name != NULL)
+        std::cerr << *file_name << ": ";
+    std::cerr << e.what() << std::endl;
+}
+
+int main(int argc, char **argv)
+{
+    Log::log.setLevel(Log::info);
+
+    po::variables_map vm = processOptions(argc, argv);
+    if (vm.count(Option::quiet))
+        Log::log.setLevel(Log::warn);
+    else if (vm.count(Option::debug))
+        Log::log.setLevel(Log::debug);
+
+    try
+    {
+        run(vm);
+        // TODO: report sorting rate
+    }
+    catch (std::ios::failure &e)
+    {
+        reportException(e);
+        return 1;
+    }
+    catch (std::runtime_error &e)
+    {
+        reportException(e);
+        return 1;
+    }
+}
--- wscript
+++ wscript
@@ -319,6 +319,19 @@ def build(bld):
             target = 'plymanifold',
             use = 'BOOST_MATH',
             install_path = None)
+    bld.program(
+            source = [
+                'sorttest.cpp',
+                'src/fast_ply.cpp',
+                'src/grid.cpp',
+                'src/statistics.cpp',
+                'src/splat_set.cpp',
+                'src/logging.cpp',
+                'src/progress.cpp',
+                'src/stxxl_log.cpp',
+                'src/timer.cpp'],
+            target = 'sorttest',
+            use = ['BOOST', 'STXXL', 'provenance'])
 
     if bld.env['XSLTPROC']:
         bld(
