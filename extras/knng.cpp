3e0a5ca2bc2a51248cffe123279959f2d0d5f672
Bruce Merry <bmerry@gmail.com>
2012-07-02 14:39:58 +0200

    Some refactoring of knng into a KDTree class

--- extras/knng.cpp
+++ extras/knng.cpp
@@ -1,7 +1,6 @@
 #if HAVE_CONFIG_H
 # include <config.h>
 #endif
-#include "knng.h"
 #include <vector>
 #include <algorithm>
 #include <cstddef>
@@ -9,56 +8,117 @@
 #include <limits>
 #include <Eigen/Core>
 #include <boost/smart_ptr/scoped_ptr.hpp>
+#include "knng.h"
+#include "../src/tr1_cstdint.h"
 
 namespace
 {
 
-struct KDPoint
+struct KDTree
 {
-    Eigen::Vector3f pos;
-    int id;
-
-    KDPoint(float x, float y, float z, int id) : pos(x, y, z), id(id) {}
-};
-
-struct KDNode
-{
-    int axis;
-    float split;
-    boost::scoped_ptr<KDNode> left, right;
-    KDPoint *first, *last;
-    float worstSquared;
-
-    KDNode() : split(0.0f), left(NULL), right(NULL), first(NULL), last(NULL) {}
-
-    bool isLeaf() const { return !left; }
-};
-
-class CompareAxis
-{
-private:
-    int axis;
-    const KDPoint *points;
-
 public:
-    CompareAxis(int axis, const KDPoint *points) : axis(axis), points(points) {}
+    typedef std::tr1::uint32_t size_type;
 
-    bool operator()(int a, int b) const
+    size_type size() const { return points.size(); }
+
+    size_type numNeighbors(size_type idx) const;
+    std::pair<float, size_type> getNeighbor(size_type idx, size_type nidx) const;
+
+    template<typename Iterator>
+    KDTree(Iterator first, Iterator last, size_type K, float maxDistanceSquared);
+
+private:
+    enum
     {
-        return points[a].pos[axis] < points[b].pos[axis];
-    }
+        MIN_LEAF = 4
+    };
+
+    struct KDPointBase
+    {
+        Eigen::Vector3f pos;
+        size_type id;
+
+        KDPointBase() {}
+        KDPointBase(float x, float y, float z, size_type id) : pos(x, y, z), id(id) {}
+    };
+
+    struct KDPoint : public KDPointBase
+    {
+        size_type numNeighbors;
+
+        KDPoint() : numNeighbors(0) {}
+        KDPoint(const KDPointBase &base) : KDPointBase(base), numNeighbors(0) {}
+    };
+
+    struct KDNode
+    {
+        int axis;       ///< Split axis, or -1 for a leaf
+        float split;    ///< Split plane value
+        size_type left, right;
+        size_type first, last;
+        float worstSquared;
+
+        KDNode() : split(0.0f), left(0), right(0), first(0), last(0) {}
+        bool isLeaf() const { return axis < 0; }
+    };
+
+    class CompareAxis
+    {
+    private:
+        int axis;
+        const KDPointBase *points;
+
+    public:
+        CompareAxis(int axis, const KDPointBase *points) : axis(axis), points(points) {}
+
+        bool operator()(size_type a, size_type b) const
+        {
+            return points[a].pos[axis] < points[b].pos[axis];
+        }
+    };
+
+    size_type K;
+    float maxDistanceSquared;
+    std::vector<KDPoint> points;
+    std::vector<KDNode> nodes;
+    std::vector<size_type> reorder; ///< Inverse of the permutation encoded in the points
+    std::vector<std::pair<float, size_type> > neighbors;
+
+    void buildTree(size_type N, KDPointBase points[], KDPointBase pointsTmp[],
+                   size_type *permute[3], size_type *permuteTmp[3],
+                   size_type *remap);
+
+    void updatePair(size_type p, size_type q);
+
+    void knngRecurse(size_type root1, float bbox1[6],
+                     size_type root2, float bbox2[6]);
 };
 
-KDNode *buildTree(int N, KDPoint points[], KDPoint pointsTmp[],
-                  int *permute[3], int *permuteTmp[3],
-                  int *remap, float worstSquared)
+KDTree::size_type KDTree::numNeighbors(size_type idx) const
 {
-    std::auto_ptr<KDNode> n(new KDNode);
+    assert(idx < points.size());
+    idx = reorder[idx];
+    return points[idx].numNeighbors;
+}
 
-    n->first = points;
-    n->last = points + N;
-    n->worstSquared = worstSquared;
-    if (N > 8)
+std::pair<float, KDTree::size_type> KDTree::getNeighbor(size_type idx, size_type nidx) const
+{
+    assert(idx < points.size());
+    idx = reorder[idx];
+    assert(nidx < points[idx].numNeighbors);
+    return neighbors[idx * K + nidx];
+}
+
+void KDTree::buildTree(size_type N, KDPointBase points[], KDPointBase pointsTmp[],
+                       size_type *permute[3], size_type *permuteTmp[3],
+                       size_type *remap)
+{
+    size_type nodeId = nodes.size();
+    nodes.push_back(KDNode());
+    KDNode &n = nodes.back();
+
+    n.worstSquared = maxDistanceSquared;
+    if (N >= 2 * MIN_LEAF) // TODO: make tunable
     {
         float maxSpread = -1.0f;
         int axis = -1;
@@ -74,23 +134,22 @@ KDNode *buildTree(int N, KDPoint points[], KDPoint pointsTmp[],
         }
         assert(axis != -1);
 
-        const int H = N / 2;
+        const size_type H = N / 2;
         float split = points[permute[axis][H]].pos[axis];
 
-        for (int i = 0; i < N; i++)
+        for (size_type i = 0; i < N; i++)
         {
             remap[permute[axis][i]] = i;
             pointsTmp[i] = points[permute[axis][i]];
         }
-        std::copy(pointsTmp, pointsTmp + N, points);
 
         for (int a = 0; a < 3; a++)
         {
-            int L = 0;
-            int R = H;
-            for (int i = 0; i < N; i++)
+            size_type L = 0;
+            size_type R = H;
+            for (size_type i = 0; i < N; i++)
             {
-                int newidx = remap[permute[a][i]];
+                size_type newidx = remap[permute[a][i]];
                 if (newidx < H)
                 {
                     permuteTmp[a][L] = newidx;
@@ -104,121 +163,103 @@ KDNode *buildTree(int N, KDPoint points[], KDPoint pointsTmp[],
             }
         }
 
-        n->split = split;
-        n->axis = axis;
-        n->left.reset(buildTree(H, points, pointsTmp, permuteTmp, permute, remap, worstSquared));
-        int *subPermute[3], *subPermuteTmp[3];
+        n.split = split;
+        n.axis = axis;
+        n.left = nodes.size();
+        buildTree(H, pointsTmp, points, permuteTmp, permute, remap);
+        // Can no longer use n: recursive call can invalidate the reference
+        size_type *subPermute[3], *subPermuteTmp[3];
         for (int j = 0; j < 3; j++)
         {
             subPermute[j] = permute[j] + H;
             subPermuteTmp[j] = permuteTmp[j] + H;
         }
-        // TODO: can we reuse temp data from part 1 to improve cache hits?
-        n->right.reset(buildTree(N - H, points + H, pointsTmp + H, subPermuteTmp, subPermute, remap + H, worstSquared));
-
-#if DEBUGGING
-        assert(n->left->first == n->first);
-        assert(n->left->last == n->first + H);
-        assert(n->right->first == n->left->last);
-        assert(n->right->last == n->last);
-        for (int i = 0; i < H; i++)
-            assert(n->left->first[i].pos[axis] <= split);
-        for (int i = 0; i < N - H; i++)
-            assert(n->right->first[i].pos[axis] >= split);
-#endif
+        nodes[nodeId].right = nodes.size();
+        buildTree(N - H, pointsTmp + H, points + H, subPermuteTmp, subPermute, remap);
     }
-    return n.release();
-}
-
-void updatePair(std::vector<std::vector<std::pair<float, int> > > &ans,
-                 KDPoint *p, const KDPoint *q, float maxDistanceSquared)
-{
-    float dist2 = (p->pos - q->pos).squaredNorm();
-    std::vector<std::pair<float, int> > &a = ans[p->id];
-    if (dist2 <= maxDistanceSquared)
+    else
     {
-        bool full = a.size() == a.capacity();
-        if (!full || dist2 < a[0].first)
+        n.first = this->points.size();
+        n.last = n.first + N;
+        n.axis = -1;
+        for (size_type i = 0; i < N; i++)
         {
-            std::pair<float, int> cur(dist2, q->id);
-            if (full)
-            {
-                std::pop_heap(a.begin(), a.end());
-                a.back() = cur;
-            }
-            else
-                a.push_back(cur);
-            std::push_heap(a.begin(), a.end());
+            this->points.push_back(points[i]);
         }
     }
 }
 
-float sqr(float x)
+void KDTree::updatePair(size_type p, size_type q)
+{
+    KDPoint &pp = points[p];
+    const KDPoint &pq = points[q];
+    float dist2 = (pp.pos - pq.pos).squaredNorm();
+
+    if (dist2 <= maxDistanceSquared)
+    {
+        bool full = pp.numNeighbors == K;
+        size_type first = p * K;
+        if (!full || dist2 < neighbors[first].first)
+        {
+            std::pair<float, size_type> cur(dist2, pq.id);
+            if (full)
+                std::pop_heap(neighbors.begin() + first, neighbors.begin() + (first + K));
+            else
+                pp.numNeighbors++;
+            neighbors[first + pp.numNeighbors - 1] = cur;
+            std::push_heap(neighbors.begin() + first, neighbors.begin() + (first + pp.numNeighbors));
+        }
+    }
+}
+
+static inline float sqr(float x)
 {
     return x * x;
 }
 
-void knngRecurse(std::vector<std::vector<std::pair<float, int> > > &ans,
-                 KDNode *root1, float bbox1[6], KDNode *root2, float bbox2[6],
-                 float maxDistanceSquared)
+void KDTree::knngRecurse(size_type root1, float bbox1[6],
+                         size_type root2, float bbox2[6])
 {
-#if DEBUGGING
-    float wtest = 0.0f;
-    for (KDPoint *i = root1->first; i != root1->last; ++i)
-    {
-        std::vector<std::pair<float, int> > &a = ans[i->id];
-        float w;
-        assert(a.capacity() == 16);
-        if (a.size() == a.capacity())
-            w = a[0].first;
-        else
-            w = maxDistanceSquared;
-        wtest = std::max(wtest, w);
-    }
-    assert(root1->worstSquared == wtest);
-#endif
+    KDNode &node1 = nodes[root1];
+    const KDNode &node2 = nodes[root2];
 
-    float dist2 = 0.0f;
+    float distSquared = 0.0f;
+    // TODO: figure out some form of incremental distance computation
     for (int i = 0; i < 3; i++)
     {
         if (bbox1[i * 2] > bbox2[i * 2 + 1])
-            dist2 += sqr(bbox1[i * 2] - bbox2[i * 2 + 1]);
-        else if (bbox1[i * 2 + 1] < bbox2[i * 2])
-            dist2 += sqr(bbox2[i * 2] - bbox1[i * 2 + 1]);
+            distSquared += sqr(bbox1[i * 2] - bbox2[i * 2 + 1]);
+        else if (bbox2[i * 2] > bbox1[i * 2 + 1])
+            distSquared += sqr(bbox2[i * 2] - bbox1[i * 2 + 1]);
     }
-    if (dist2 > root1->worstSquared)
+    if (distSquared > node1.worstSquared)
         return;
 
-    if (root1->isLeaf() && root2->isLeaf())
+    if (node1.isLeaf() && node2.isLeaf())
     {
         float wmax = 0.0f;
-        for (KDPoint *i = root1->first; i != root1->last; ++i)
+        for (size_t i = node1.first; i != node1.last; ++i)
         {
-#if DEBUGGING
-            assert(i->pos[0] >= bbox1[0] && i->pos[0] <= bbox1[1]);
-            assert(i->pos[1] >= bbox1[2] && i->pos[1] <= bbox1[3]);
-            assert(i->pos[2] >= bbox1[4] && i->pos[2] <= bbox1[5]);
-#endif
-            for (const KDPoint *j = root2->first; j != root2->last; ++j)
+            for (size_t j = node2.first; j != node2.last; ++j)
                 if (j != i)
-                    updatePair(ans, i, j, maxDistanceSquared);
-            std::vector<std::pair<float, int> > &a = ans[i->id];
+                    updatePair(i, j);
+            const KDPoint &pi = points[i];
             float w;
-            if (a.size() == a.capacity())
-                w = a[0].first;
+            if (pi.numNeighbors == K)
+                w = neighbors[pi.id * K].first;
             else
                 w = maxDistanceSquared;
             if (w > wmax)
                 wmax = w;
         }
-        root1->worstSquared = wmax;
+        node1.worstSquared = wmax;
     }
     else
     {
         bool split2;
-        if (root1->isLeaf())
+        if (node1.isLeaf())
             split2 = true;
-        else if (root2->isLeaf())
+        else if (node2.isLeaf())
             split2 = false;
         else
         {
@@ -229,114 +270,133 @@ void knngRecurse(std::vector<std::vector<std::pair<float, int> > > &ans,
 
         if (split2)
         {
-            int axis = root2->axis;
+            int axis = node2.axis;
             int bl = 2 * axis;
             int br = bl + 1;
-            float split = root2->split;
+            float split = node2.split;
             float mid = 0.5 * (bbox1[bl] + bbox1[br]);
             float L = bbox2[bl];
             float R = bbox2[br];
             if (mid < split)
             {
                 bbox2[br] = split;
-                knngRecurse(ans, root1, bbox1, root2->left.get(), bbox2, maxDistanceSquared);
+                knngRecurse(root1, bbox1, node2.left, bbox2);
                 bbox2[br] = R;
                 bbox2[bl] = split;
-                knngRecurse(ans, root1, bbox1, root2->right.get(), bbox2, maxDistanceSquared);
+                knngRecurse(root1, bbox1, node2.right, bbox2);
                 bbox2[bl] = L;
             }
             else
             {
                 bbox2[bl] = split;
-                knngRecurse(ans, root1, bbox1, root2->right.get(), bbox2, maxDistanceSquared);
+                knngRecurse(root1, bbox1, node2.right, bbox2);
                 bbox2[bl] = L;
                 bbox2[br] = split;
-                knngRecurse(ans, root1, bbox1, root2->left.get(), bbox2, maxDistanceSquared);
+                knngRecurse(root1, bbox1, node2.left, bbox2);
                 bbox2[br] = R;
             }
         }
         else
         {
-            int axis = root1->axis;
+            int axis = node1.axis;
             int bl = 2 * axis;
             int br = 2 * axis + 1;
-            float split = root1->split;
+            float split = node1.split;
             float mid = 0.5 * (bbox2[bl] + bbox2[br]);
             float L = bbox1[bl];
             float R = bbox1[br];
             if (mid < split)
             {
                 bbox1[br] = split;
-                knngRecurse(ans, root1->left.get(), bbox1, root2, bbox2, maxDistanceSquared);
+                knngRecurse(node1.left, bbox1, root2, bbox2);
                 bbox1[br] = R;
                 bbox1[bl] = split;
-                knngRecurse(ans, root1->right.get(), bbox1, root2, bbox2, maxDistanceSquared);
+                knngRecurse(node1.right, bbox1, root2, bbox2);
                 bbox1[bl] = L;
             }
             else
             {
                 bbox1[bl] = split;
-                knngRecurse(ans, root1->right.get(), bbox1, root2, bbox2, maxDistanceSquared);
+                knngRecurse(node1.right, bbox1, root2, bbox2);
                 bbox1[bl] = L;
                 bbox1[br] = split;
-                knngRecurse(ans, root1->left.get(), bbox1, root2, bbox2, maxDistanceSquared);
+                knngRecurse(node1.left, bbox1, root2, bbox2);
                 bbox1[br] = R;
             }
 
-            root1->worstSquared = std::max(root1->left->worstSquared, root1->right->worstSquared);
+            node1.worstSquared = std::max(nodes[node1.left].worstSquared,
+                                          nodes[node1.right].worstSquared);
         }
     }
+}
+
+template<typename Iterator>
+KDTree::KDTree(Iterator first, Iterator last, size_type K, float maxDistanceSquared)
+    : K(K), maxDistanceSquared(maxDistanceSquared)
+{
+    std::vector<KDPointBase> pointData[2];
+    std::vector<size_type> permuteData[2][3];
+    size_type *permute[2][3];
+
+    // Reserve storage
+    size_type N = last - first;
+    points.reserve(N);
+    neighbors.resize(N * K);
+    nodes.reserve(2 * (N / MIN_LEAF) + 1);
+
+    // Create transient data structures
+    pointData[0].reserve(N);
+    size_type idx = 0;
+    for (Iterator i = first; i != last; ++i, ++idx)
+        pointData[0].push_back(KDPointBase(i->position[0], i->position[1], i->position[2], idx));
+    pointData[1].resize(N); // arbitrary data
+
+    for (int axis = 0; axis < 3; axis++)
+    {
+        permuteData[1][axis].resize(N); // arbitrary data
+        permuteData[0][axis].reserve(N);
+        for (size_type i = 0; i < N; i++)
+            permuteData[0][axis].push_back(i);
+        std::sort(permuteData[0][axis].begin(), permuteData[0][axis].end(), CompareAxis(axis, &pointData[0][0]));
+
+        permute[0][axis] = &permuteData[0][axis][0];
+        permute[1][axis] = &permuteData[1][axis][0];
+    }
+
+    std::vector<size_type> remap(N); // arbitrary data
+
+    buildTree(N, &pointData[0][0], &pointData[1][0], permute[0], permute[1], &remap[0]);
+    reorder.resize(N);
+    for (size_type i = 0; i < N; i++)
+        reorder[points[i].id] = i;
+
+    float bbox[2][6];
+    for (int i = 0; i < 2; i++)
+        for (int j = 0; j < 3; j++)
+        {
+            bbox[i][2 * j] = -std::numeric_limits<float>::infinity();
+            bbox[i][2 * j + 1] = std::numeric_limits<float>::infinity();
+        }
+    knngRecurse(0, bbox[0], 0, bbox[1]);
 }
 
 } // anonymous namespace
 
 std::vector<std::vector<std::pair<float, int> > > knng(const Statistics::Container::vector<Splat> &splats, int K, float maxDistanceSquared)
 {
-    int N = splats.size();
-    std::vector<KDPoint> points, pointsTmp;
-    std::vector<int> permuteData[6];
-    points.reserve(N);
-    pointsTmp.reserve(N); // TODO: needs to be populated
-    for (int j = 0; j < 3; j++)
-    {
-        permuteData[j].reserve(N);
-        permuteData[j + 3].resize(N); // tmp array
-    }
-    for (int i = 0; i < N; i++)
-    {
-        const Splat &s = splats[i];
-        points.push_back(KDPoint(s.position[0], s.position[1], s.position[2], i));
-        for (int j = 0; j < 3; j++)
-            permuteData[j].push_back(i);
-    }
+    KDTree tree(splats.begin(), splats.end(), K, maxDistanceSquared);
 
-    int *permute[3];
-    int *permuteTmp[3];
-    for (int j = 0; j < 3; j++)
+    std::vector<std::vector<std::pair<float, int> > > ans;
+    ans.reserve(tree.size());
+    for (KDTree::size_type i = 0; i < tree.size(); i++)
     {
-        std::sort(permuteData[j].begin(), permuteData[j].end(), CompareAxis(j, &points[0]));
-        permute[j] = &permuteData[j][0];
-        permuteTmp[j] = &permuteData[j + 3][0];
+        int k = tree.numNeighbors(i);
+        ans.push_back(std::vector<std::pair<float, int> >(k));
+        for (int j = 0; j < k; j++)
+        {
+            std::pair<float, KDTree::size_type> n = tree.getNeighbor(i, j);
+            ans[i][j] = std::make_pair(n.first, int(n.second));
+        }
     }
-    std::vector<int> remap(N);
-    boost::scoped_ptr<KDNode> root(buildTree(N, &points[0], &pointsTmp[0],
-                                             permute, permuteTmp,
-                                             &remap[0], maxDistanceSquared));
-
-    // TODO: clear the temporary arrays
-
-    std::vector<std::vector<std::pair<float, int> > > ans(N);
-    for (int i = 0; i < N; i++)
-        ans[i].reserve(K);
-
-    float bbox1[6];
-    float bbox2[6];
-    for (int j = 0; j < 3; j++)
-    {
-        bbox1[2 * j] = -std::numeric_limits<float>::infinity();
-        bbox1[2 * j + 1] = std::numeric_limits<float>::infinity();
-    }
-    std::copy(bbox1, bbox1 + 6, bbox2);
-    knngRecurse(ans, root.get(), bbox1, root.get(), bbox2, maxDistanceSquared);
     return ans;
 }
