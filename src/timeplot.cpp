bdf33fe0ecb4967877e7070a65f8170c10a08105
Bruce Merry <bmerry@gmail.com>
2012-08-15 13:45:23 +0200

    Actually add the timeplot sources (oops)

---
+++ src/timeplot.cpp
@@ -0,0 +1,125 @@
+/**
+ * @file
+ *
+ * Record timing information.
+ */
+
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/locks.hpp>
+#include <boost/lexical_cast.hpp>
+#include "timeplot.h"
+#include "statistics.h"
+#include "timer.h"
+#include "errors.h"
+
+namespace Timeplot
+{
+
+static boost::mutex outputMutex;
+static Timer::timestamp startTime;
+static std::ofstream log;
+
+void init()
+{
+    startTime = Timer::currentTime();
+    log.open("timeplot.log");
+    log << std::fixed;
+    log.precision(9);
+}
+
+Worker::Worker(const std::string &name) : name(name), currentAction(NULL)
+{
+}
+
+Worker::Worker(const std::string &name, int idx)
+    : name(name + "." + boost::lexical_cast<std::string>(idx)),
+    currentAction(NULL)
+{
+}
+
+Action *Worker::start(Action *current, Timer::timestamp time)
+{
+    Action *ret = currentAction;
+    currentAction = current;
+    if (ret != NULL)
+        ret->pause(time);
+    return ret;
+}
+
+void Worker::stop(Action *current, Action *prev, Timer::timestamp time)
+{
+    (void) current; // prevent warning in release builds
+    MLSGPU_ASSERT(current == currentAction, state_error);
+    currentAction = prev;
+    if (currentAction != NULL)
+        currentAction->resume(time);
+}
+
+const std::string &Worker::getName() const
+{
+    return name;
+}
+
+void Action::init()
+{
+    start = Timer::currentTime();
+    running = true;
+    elapsed = 0.0;
+    oldAction = worker.start(this, start);
+}
+
+Action::Action(const std::string &name, Worker &worker)
+    : name(name), worker(worker), stat(NULL)
+{
+    init();
+}
+
+Action::Action(const std::string &name, Worker &worker, Statistics::Variable &stat)
+    : name(name), worker(worker), stat(&stat)
+{
+    init();
+}
+
+Action::Action(const std::string &name, Worker &worker, const std::string &statName)
+    : name(name), worker(worker),
+    stat(&Statistics::getStatistic<Statistics::Variable>(statName))
+{
+    init();
+}
+
+void Action::pause(Timer::timestamp time)
+{
+    MLSGPU_ASSERT(running, state_error);
+    running = false;
+    elapsed += Timer::getElapsed(start, time);
+
+    boost::lock_guard<boost::mutex> lock(outputMutex);
+    log << "EVENT " << worker.getName() << ' ' << name << ' '
+        << Timer::getElapsed(startTime, start) << ' '
+        << Timer::getElapsed(startTime, time) << '\n';
+}
+
+void Action::resume(Timer::timestamp time)
+{
+    MLSGPU_ASSERT(!running, state_error);
+    running = true;
+    start = time;
+}
+
+Action::~Action()
+{
+    Timer::timestamp stop = Timer::currentTime();
+    pause(stop);
+    if (stat != NULL)
+        stat->add(elapsed);
+    worker.stop(this, oldAction, stop);
+}
+
+} // namespace Timeplot
---
+++ src/timeplot.h
@@ -0,0 +1,106 @@
+/**
+ * @file
+ *
+ * Record timing information.
+ */
+
+#ifndef TIMEPLOT_H
+#define TIMEPLOT_H
+
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
+#include <boost/noncopyable.hpp>
+#include <string>
+#include "timer.h"
+#include "statistics.h"
+
+namespace Timeplot
+{
+
+/**
+ * Initialize the timeplot subsystem. This must be called before any other
+ * function in this namespace, to set a zero time.
+ */
+void init();
+
+class Action;
+
+/**
+ * Encapsulates a worker. Workers perform actions, which must start and stop in
+ * LIFO order i.e. it emulates a call stack. However, only the leaf call is shown
+ * in the resulting plot.
+ */
+class Worker : public boost::noncopyable
+{
+    friend class Action;
+private:
+    const std::string name;
+    Action *currentAction;
+
+    /**
+     * Called by @ref Action to push itself onto the stack. It returns the previous
+     * action, which it must save and pass back to @ref stop. That is, the stack is
+     * maintained as a linked list of each action pointing to the previous one.
+     */
+    Action *start(Action *current, Timer::timestamp time);
+
+    /**
+     * Called by @ref Action to pop itself from the stack. It must pass itself and
+     * the previous action in the stack.
+     *
+     * @pre @a current is the current action.
+     */
+    void stop(Action *current, Action *prev, Timer::timestamp time);
+public:
+    /**
+     * Construct the worker, in the stopped state.
+     *
+     * @param name Name for the worker
+     */
+    explicit Worker(const std::string &name);
+
+    /**
+     * Construct the worker, in the stopped state.
+     *
+     * @param name Base name for the worker
+     * @param idx  Worker number that is appended to the name
+     */
+    Worker(const std::string &name, int idx);
+
+    const std::string &getName() const;
+};
+
+/**
+ * A single action taken by a worker. This is similar to @ref Statistics::Timer,
+ * in that it records a start time on creation and a stop time on destruction.
+ * It can optionally record to a statistic in the same way.
+ */
+class Action : public boost::noncopyable
+{
+    friend class Worker;
+private:
+    const std::string name;
+    Worker &worker;
+    Statistics::Variable *stat;
+    Action *oldAction;       ///< Action below this in the stack
+
+    bool running;            ///< Whether time is being attributed to this action
+    double elapsed;          ///< Time taken up to the last time it was paused
+    Timer::timestamp start;  ///< Time of the last resume, or of construction
+
+    void init();
+    void pause(Timer::timestamp time);
+    void resume(Timer::timestamp time);
+
+public:
+    Action(const std::string &name, Worker &worker);
+    Action(const std::string &name, Worker &worker, Statistics::Variable &stat);
+    Action(const std::string &name, Worker &worker, const std::string &statName);
+
+    ~Action();
+};
+
+} // namespace Timeplot
+
+#endif /* !TIMEPLOT_H */
