97f7b5d195849a21fdc3e8f7708cf8b0e46b6b78
Bruce Merry <bmerry@gmail.com>
2012-10-09 11:58:39 +0200

    Exclude /usr/local/include from coverage reports

--- utils/mkcoverage.sh
+++ utils/mkcoverage.sh
@@ -8,6 +8,7 @@ mpirun -n 2 build/testmpi --test=commit
 lcov --capture --directory build --base $PWD/build -o build/coverage_full.info
 lcov --remove build/coverage_full.info \
     '/usr/include/*' \
+    '/usr/local/include/*' \
     '*/test.cpp' \
     '*/CL/*.h*' -o build/coverage.info
 genhtml --prefix="$(dirname $PWD)" --demangle-cpp -o build/coverage build/coverage.info
