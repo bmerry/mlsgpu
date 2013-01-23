a0c547ffb6e26bb349fd28e7a6c3df123600982a
Bruce Merry <bmerry@gmail.com>
2013-01-23 18:02:30 +0200

    Include bucket write time in analyze_stats

--- utils/analyze_stats.py
+++ utils/analyze_stats.py
@@ -69,6 +69,8 @@ def analyze_bucket_fine(values):
     threads = values['total-bucket-threads']
     times['startup'] = values['bucket.fine.pop.first']
     times['compute'] = values['bucket.fine.compute']
+    if 'bucket.fine.write' in values:
+        times['write'] = values['bucket.fine.write']
     times['wait-in'] = values['bucket.fine.pop']
     times['wait-out'] = values['device.get'] + values.get('device.push', 0.0)
     print_breakdown('FineBucket', times, values['pass1.time'] * threads)
