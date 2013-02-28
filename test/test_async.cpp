7a91b83309562866a6b8aeefea44d7f18c338c99
Bruce Merry <bmerry@gmail.com>
2013-02-28 13:14:53 +0200

    Add test for AsyncWriter

---
+++ test/test_async.cpp
@@ -0,0 +1,105 @@
+/**
+ * @file
+ *
+ * Tests for @ref AsyncWriter.
+ */
+
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <boost/smart_ptr/shared_ptr.hpp>
+#include <boost/smart_ptr/make_shared.hpp>
+#include <boost/smart_ptr/scoped_ptr.hpp>
+#include <boost/filesystem/path.hpp>
+#include <boost/filesystem/operations.hpp>
+#include <boost/tr1/random.hpp>
+#include "testutil.h"
+#include "../src/binary_io.h"
+#include "../src/async_io.h"
+#include "../src/misc.h"
+#include "../src/tr1_cstdint.h"
+
+class TestAsyncWriter : public CppUnit::TestFixture
+{
+    CPPUNIT_TEST_SUITE(TestAsyncWriter);
+    CPPUNIT_TEST(testStress);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    virtual void setUp();    ///< Obtain filename for temporary file
+    virtual void tearDown(); ///< Wipe out the output file
+
+private:
+    boost::filesystem::path filename;
+
+    void testStress();   ///< Make lots of writes to file, check that they arrive
+};
+CPPUNIT_TEST_SUITE_NAMED_REGISTRATION(TestAsyncWriter, TestSet::perNightly());
+
+void TestAsyncWriter::setUp()
+{
+    boost::filesystem::ofstream dummy;
+    createTmpFile(filename, dummy);
+}
+
+void TestAsyncWriter::tearDown()
+{
+    boost::filesystem::remove(filename);
+}
+
+void TestAsyncWriter::testStress()
+{
+    Timeplot::Worker tworker("test");
+    typedef std::tr1::uint32_t value_type;
+    const value_type size = 1100000000; // file will be larger than 4GiB
+    const std::size_t bufferSize = 64 * 1024;
+
+    boost::shared_ptr<BinaryWriter> writer(createWriter(SYSCALL_WRITER));
+    writer->open(filename);
+    writer->resize(BinaryWriter::offset_type(size) * sizeof(value_type));
+
+    AsyncWriter async(4, bufferSize * sizeof(value_type));
+    async.start();
+
+    std::tr1::mt19937 engine;
+    std::tr1::uniform_int<int> dist(1, bufferSize / 2);
+    value_type pos = 0;
+    while (pos < size)
+    {
+        value_type chunk = dist(engine);
+        if (pos + chunk > size)
+            chunk = size - pos;
+        boost::shared_ptr<AsyncWriterItem> item = async.get(tworker, chunk * sizeof(value_type));
+        value_type *ptr = reinterpret_cast<value_type *>(item->get());
+        for (value_type i = 0; i < chunk; i++)
+            ptr[i] = i + pos;
+        async.push(tworker, item, writer,
+                   chunk * sizeof(value_type),
+                   BinaryWriter::offset_type(pos) * sizeof(value_type));
+        pos += chunk;
+    }
+    writer.reset();
+    async.stop();
+
+    boost::scoped_ptr<BinaryReader> reader(createReader(SYSCALL_READER));
+    reader->open(filename);
+    value_type buffer[bufferSize];
+    pos = 0;
+    while (pos < size)
+    {
+        std::size_t read = reader->read(
+            buffer,
+            bufferSize * sizeof(value_type),
+            BinaryWriter::offset_type(pos) * sizeof(value_type));
+        CPPUNIT_ASSERT(read > 0 && read % sizeof(value_type) == 0);
+        read /= sizeof(value_type);
+        for (value_type i = 0; i < read; i++)
+            CPPUNIT_ASSERT_EQUAL(i + pos, buffer[i]);
+        pos += read;
+    }
+    CPPUNIT_ASSERT_EQUAL(size, pos);
+    reader->close();
+}
